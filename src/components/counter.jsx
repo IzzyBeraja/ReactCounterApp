d8fdf863c8aa6b7bd1ba9abf13b212a7c619a263
IzzyBeraja <20325290+IzzyBeraja@users.noreply.github.com>
2020-07-25 02:38:43 -0400

    Created a new tree of components. Updated the gitignore for vscode.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-	state = {
-		count: 1,
-		tags: ["tag1", "tag2", "tag3"],
-	};
-
 	render() {
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>
 					{this.formatCount()}
 				</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
-				<ul>
-					{this.state.tags.map((tag) => (
-						<li key={tag}>{tag}</li>
-					))}
-				</ul>
+				<button
+					onClick={() => this.props.onIncrement(this.props.counter)}
+					className="btn btn-secondary btn-sm">
+					Increment
+				</button>
+				<button
+					onClick={() => this.props.onDelete(this.props.counter)}
+					className="btn btn-danger btn-sm m-2">
+					Delete
+				</button>
 			</div>
 		);
 	}
 
 	getBadgeClasses() {
 		let classes = "badge m-2 ";
-		classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+		classes +=
+			this.props.counter.value === 0 ? "badge-warning" : "badge-primary";
 		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? "Zero" : count;
+		const { value } = this.props.counter;
+		return value === 0 ? "Zero" : value;
 	}
 }
 
