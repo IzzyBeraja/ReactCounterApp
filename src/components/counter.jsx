39d4d42a1dde2ba3a09ec083c2d32cc87950aa29
IzzyBeraja <20325290+IzzyBeraja@users.noreply.github.com>
2020-07-26 04:46:00 -0400

    Added a NavBar. Pushed data from the counters component to App.js Learned how to destructure and refactore some parts.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 
 class Counter extends Component {
 	render() {
+        const { onIncrement, counter, onDelete } = this.props;
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>
 					{this.formatCount()}
 				</span>
 				<button
-					onClick={() => this.props.onIncrement(this.props.counter)}
+                    onClick={() => onIncrement(counter)}
 					className="btn btn-secondary btn-sm">
 					Increment
 				</button>
 				<button
-					onClick={() => this.props.onDelete(this.props.counter)}
+					onClick={() => onDelete(counter)}
 					className="btn btn-danger btn-sm m-2">
 					Delete
 				</button>
@@ -22,10 +23,7 @@ class Counter extends Component {
 	}
 
 	getBadgeClasses() {
-		let classes = "badge m-2 ";
-		classes +=
-			this.props.counter.value === 0 ? "badge-warning" : "badge-primary";
-		return classes;
+        return "badge m-2 " + (this.props.counter.value === 0 ? "badge-warning" : "badge-primary");
 	}
 
 	formatCount() {
